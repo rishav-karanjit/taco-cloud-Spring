9a0562e159c285dc6e52c4e46252b05ab5214be4
Rishav Karanjit <karanjitrishav4@gmail.com>
2023-05-16 09:18:34 -0700

    Added initial commit

---
+++ src/main/java/sia/tacocloud/DesignTacoController.java
@@ -0,0 +1,72 @@
+package sia.tacocloud;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.stream.Collectors;
+import sia.tacocloud.Ingredient;
+import sia.tacocloud.Ingredient.Type;
+@Slf4j
+@Controller
+@RequestMapping("/design")
+@SessionAttributes("tacoOrder")
+
+public class DesignTacoController {
+
+    @ModelAttribute
+    public void addIngredientsToModel(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+                 new Ingredient("CHED", "Cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE)
+
+        );
+
+        Ingredient.Type[] types = Ingredient.Type.values();
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
+        }
+    }
+
+    @ModelAttribute(name = "tacoOrder")
+    public TacoOrder order() {
+        return new TacoOrder();
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
+    }
+
+    @GetMapping
+    public String showDesignForm() {
+        return "design";
+    }
+
+    @PostMapping
+    public String processTaco(Taco taco,
+                              @ModelAttribute TacoOrder tacoOrder) {
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+        return "redirect:/orders/current";
+    }
+
+    private Iterable<Ingredient> filterByType(
+            List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+
+}
---
+++ src/main/java/sia/tacocloud/IngredientByIdConverter.java
@@ -0,0 +1,37 @@
+package sia.tacocloud;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import sia.tacocloud.Ingredient;
+import sia.tacocloud.Ingredient.Type;
+
+@Component
+public class IngredientByIdConverter implements Converter<String, Ingredient> {
+
+    private Map<String, Ingredient> ingredientMap = new HashMap<>();
+
+    public IngredientByIdConverter() {
+        ingredientMap.put("FLTO",
+                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+        ingredientMap.put("COTO",
+                new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+        ingredientMap.put("GRBF",
+                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+        ingredientMap.put("CARN",
+                new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+        ingredientMap.put("CHED",
+                new Ingredient("CHED", "Cheddar", Type.CHEESE));
+        ingredientMap.put("JACK",
+                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+
+    }
+
+    @Override
+    public Ingredient convert(String id) {
+        return ingredientMap.get(id);
+    }
+
+}
